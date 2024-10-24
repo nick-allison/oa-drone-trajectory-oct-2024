a2267b24fcc172fe13df45ffd9e1f53dbe3ed09b
Nicholas <nallison@hawk.iit.edu>
2024-10-23 23:56:18 -0500

    Week 2 tasks + bonus

--- src/camera_utils.py
+++ src/camera_utils.py
@@ -30,7 +30,13 @@ def project_world_point_to_image(camera: Camera, point: np.ndarray) -> np.ndarra
     Returns:
         np.ndarray: [u, v] pixel coordinates corresponding to the point.
     """
-    raise NotImplementedError()
+    x = camera.fx * (point[0] / point[2])
+    y = camera.fy * (point[1] / point[2])
+
+    u = x + camera.cx
+    v = y + camera.cy
+    
+    return np.array([u, v], dtype=np.float32)
 
 
 def compute_image_footprint_on_surface(camera: Camera, distance_from_surface: float) -> np.ndarray:
@@ -43,7 +49,10 @@ def compute_image_footprint_on_surface(camera: Camera, distance_from_surface: fl
     Returns:
         np.ndarray: [footprint_x, footprint_y] in meters.
     """
-    raise NotImplementedError()
+    X = (camera.image_size_x * distance_from_surface) / camera.fx
+    Y = (camera.image_size_y * distance_from_surface) / camera.fy
+
+    return np.array([X, Y], dtype=np.float32) 
 
 def compute_ground_sampling_distance(camera: Camera, distance_from_surface: float) -> float:
     """Compute the ground sampling distance (GSD) at a given distance from the surface.
@@ -55,4 +64,25 @@ def compute_ground_sampling_distance(camera: Camera, distance_from_surface: floa
     Returns:
         float: the GSD in meters (smaller among x and y directions).
     """
-    raise NotImplementedError()
+    X = (camera.image_size_x * distance_from_surface) / camera.fx
+    return X / camera.image_size_x
+
+def reproject_image_point_to_world(camera: Camera, point: np.ndarray, distance: float) -> np.ndarray:
+    """Compute the reprojected point a 2d projected image point and the distance from the surface.
+
+    Args:
+        camera (Camera): the camera model.
+        point (np.ndarray): the 2D projected image point
+        distance (float): distance from the surface (in m).
+    
+    Returns:
+        np.ndarray: [X, Y, Z] reprojected 3D point.
+    """
+    x = point[0] - camera.cx
+    y = point[1] - camera.cy
+
+    X = (x * distance) / camera.fx
+    Y = (y * distance) / camera.fy
+    Z = distance
+
+    return np.array([X, Y, Z], dtype=np.float32) 
--- src/data_model.py
+++ src/data_model.py
@@ -10,7 +10,14 @@ class Camera:
     - https://github.com/colmap/colmap/blob/3f75f71310fdec803ab06be84a16cee5032d8e0d/src/colmap/sensor/models.h#L220
     - https://en.wikipedia.org/wiki/Pinhole_camera_model
     """
-    pass
+    fx: float
+    fy: float
+    cx: float
+    cy: float
+    sensor_size_x_mm: float
+    sensor_size_y_mm: float
+    image_size_x: int
+    image_size_y: int
 
 @dataclass
 class DatasetSpec:
